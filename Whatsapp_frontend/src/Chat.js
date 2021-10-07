75bb4b53634e360a0f35ffd6b800df7665183e7f
Vudutha Venkata Surya Kiran <67712163+venkatasurya777@users.noreply.github.com>
2021-10-07 13:08:06 +0530

    Add files via upload

---
+++ Whatsapp_frontend/src/Chat.js
@@ -0,0 +1,72 @@
+import { Avatar, IconButton } from "@material-ui/core";
+import { AttachFile, SearchOutlined } from "@material-ui/icons";
+import MoreVert from "@material-ui/icons/MoreVert";
+import React, { useState } from "react";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import "./Chat.css";
+import MicIcon from "@material-ui/icons/Mic";
+import axios from "./axios";
+
+function Chat({ messages }) {
+  const [input, setInput] = useState("");
+  const sendMessage = async (e) => {
+    e.preventDefault();
+
+    await axios.post("/messages/new", {
+      message: input,
+      name: "Ananth",
+      timestamp: "Just now",
+      received: false,
+    });
+    setInput("");
+  };
+  return (
+    <div className="chat">
+      <div className="chat_header">
+        <Avatar />
+        <div className="chat_headerInfo">
+          <h3>Room name</h3>
+          <p>Last seen at...</p>
+        </div>
+        <div className="chat_headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+      <div className="chat_body">
+        {messages.map((message) => (
+          <p className={`chat_message ${message.received && "chat_reciever"}`}>
+            <span className="chat_name">{message.name}</span>
+            {message.message}
+            <span className="chat_timestamp">{message.timestamp}</span>
+          </p>
+        ))}
+      </div>
+
+      <div className="chat_footer">
+        <InsertEmoticonIcon />
+        <form>
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            type="text"
+            placeholder="Type a message"
+          />
+          <button onClick={sendMessage} type="submit">
+            Send a message
+          </button>
+        </form>
+        <MicIcon />
+      </div>
+    </div>
+  );
+}
+
+export default Chat;
---
+++ Whatsapp_frontend/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC0QUCnllkLOV0MArZq3RtZUE-RNyZQtHE",
+    authDomain: "whatsapp-58037.firebaseapp.com",
+    projectId: "whatsapp-58037",
+    storageBucket: "whatsapp-58037.appspot.com",
+    messagingSenderId: "500638501062",
+    appId: "1:500638501062:web:8fd05d7d21a241a2872fc7"
+  };
